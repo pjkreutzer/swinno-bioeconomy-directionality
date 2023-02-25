cf0359848d8086834c5b86e12d9f848231424c8f
Philipp Jonas Kreutzer <philipp_jonas.kreutzer@ekh.lu.se>
2023-02-25 12:48:00 +0100

    update paper

--- src/categorization_helpers.py
+++ src/categorization_helpers.py
@@ -7,19 +7,22 @@ def clean_codes(df, column, code_digits, remove_whitespace=True, drop_na=True):
     if drop_na:
         df = df.dropna(subset=[column])
 
-    df[column] = df[column].apply(
+    df.loc[:, column] = df[column].astype(str)
+
+    df.loc[:, column] = df[column].apply(
         lambda x: re.sub(r"\b(0)\b", r"0" * code_digits, x)
     )
-    df[column] = df[column].apply(
+    df.loc[:, column] = df[column].apply(
         lambda x: re.sub(r"\b(9)\b", r"9" * code_digits, x)
     )
 
-    df[column] = df[column].apply(
+    df.loc[:, column] = df[column].apply(
         lambda x: re.sub(rf"(\d{ {code_digits} })(?!$)", r"\1,", str(x))
     )
 
     if remove_whitespace:
-        df[column] = df[column].apply(lambda x: re.sub(r"\s", "", x))
+        df.loc[:, column] = df[column].apply(lambda x: re.sub(r"\s", "", x))
+        df.loc[:, column] = df[column].str.strip()
 
     return df
 
