9216ac35b632a0ae5497786802c63425694b1f3e
Philipp Jonas Kreutzer <philipp_jonas.kreutzer@ekh.lu>
2024-09-12 16:27:32 +0200

    add script to enter all classifications into db

---
+++ swinno_bioeconomy_directionality/config.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from loguru import logger
+
+# Paths
+PROJ_ROOT = Path(__file__).resolve().parents[1]
+logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
+
+DATA_DIR = PROJ_ROOT / "data"
+RAW_DATA_DIR = DATA_DIR / "raw"
+INTERIM_DATA_DIR = DATA_DIR / "interim"
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
+EXTERNAL_DATA_DIR = DATA_DIR / "external"
+
+RESULTS_DIR = PROJ_ROOT / "results"
+FIGURES_DIR = RESULTS_DIR / "figures"
+
+# If tqdm is installed, configure loguru with tqdm.write
+# https://github.com/Delgan/loguru/issues/135
+try:
+    from tqdm import tqdm
+
+    logger.remove(0)
+    logger.add(lambda msg: tqdm.write(msg, end=""), colorize=True)
+except ModuleNotFoundError:
+    pass
